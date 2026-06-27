8904f6afca1b1c890a858822197330ed824d5873
pauxdev <heypaux@gmail.com>
2026-06-26 23:19:42 -0500

    Corrección de la eñe en la variable

--- supabase.js
+++ supabase.js
@@ -4,7 +4,7 @@
 
 const SUPABASE_URL = 'https://yhfqfywlkomsnorapgtk.supabase.co';         // ← reemplaza
 const SUPABASE_ANON_KEY = 'sb_publishable_sLa68n5Hj74NAAaC9AOjYw_LnyKiytC';         // ← reemplaza
-const ADMIN_PASSWORD = 'CONTRASEÑA_SECRETA_EN_VERCEL';                    // ← CAMBIA ESTO
+const ADMIN_PASSWORD = 'CONTRASENA_SECRETA_EN_VERCEL';                    // ← CAMBIA ESTO
 
 const { createClient } = supabase;
 const db = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
